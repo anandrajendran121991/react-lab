35ecb94714bf2037c18c2011ecf7a626d702a2da
anandrajendran121991 <anand.rajendran121991@gmail.com>
2025-08-30 22:07:54 +0530

    remove secrets from firebase component

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAo1Zn-C4tbqw0_d8RftdaQsJOddLmdwmk",
-  authDomain: "react-lab-9c611.firebaseapp.com",
-  projectId: "react-lab-9c611",
-  storageBucket: "react-lab-9c611.firebasestorage.app",
-  messagingSenderId: "492895146550",
-  appId: "1:492895146550:web:f7b323fec90251f51b7e97",
-  measurementId: "G-0M46Q340CW",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
